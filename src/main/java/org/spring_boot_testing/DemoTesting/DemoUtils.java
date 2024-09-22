1024f0d649fa5af4d0d0be40ad4c6452ed99f919
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-23 00:24:31 +0530

    Adding Junit dependency and writing first Junit Test Script.

--- src/main/java/org/spring_boot_testing/DemoTesting/DemoUtils.java
+++ src/main/java/org/spring_boot_testing/DemoTesting/DemoUtils.java
@@ -1,4 +1,13 @@
 package org.spring_boot_testing.DemoTesting;
 
 public class DemoUtils {
+    public int addition(int a,int b){
+        return a+b;
+    }
+    public Object nullCheck(Object object){
+        if(object!=null){
+            return object;
+        }
+        return null;
+    }
 }
--- src/test/java/spring_boot_testing/DemoTesting/DemoUtilsTest.java
+++ src/test/java/spring_boot_testing/DemoTesting/DemoUtilsTest.java
@@ -1,5 +1,15 @@
 package spring_boot_testing.DemoTesting;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.spring_boot_testing.DemoTesting.DemoUtils;
+
 class DemoUtilsTest {
 
+    @Test
+    public void equalNotEqualTest(){
+        DemoUtils demoUtils=new DemoUtils();
+        int expected=10;
+        Assertions.assertEquals(expected,demoUtils.addition(4,6),"addition of 4,6 must be 10");
+    }
 }
